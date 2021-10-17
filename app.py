0e71584a83827c6de73783bce30ea65bf4f30caf
andrew tomich <johnatomich@gmail.com>
2021-10-17 18:32:51 -0500

    Update config.py, requirements.txt

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import PGapp
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -1,20 +1,29 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     DEBUG = False
-    DEVELOPMENT = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "skey")
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'skey'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
 
 
 class StagingConfig(Config):
+    DEVELOPEMENT = True 
     DEBUG = True
 
 
 class DevelopmentConfig(Config):
-    DEBUG = True
     DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
