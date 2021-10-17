e57731c35132044ae253de67729ee2f3c3426df8
andrew tomich <johnatomich@gmail.com>
2021-10-17 18:12:18 -0500

    Add config.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route('/')
 def hello():
-    return "Hello, you!"
+    secret_key = app.config.get("SECRET_KEY") 
+    return f"The skey is {secret_key}"
