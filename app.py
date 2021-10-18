bf15213a2b063faa3148d1c2c85982bc3011a4ce
andrew tomich <johnatomich@gmail.com>
2021-10-18 02:50:38 -0500

    Add index.html and counting logic in app.py, stop_words.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from stop_words import stops
+from collections import Counter
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -9,15 +16,51 @@ db = SQLAlchemy(app)
 
 from models import PGapp
 
-@app.route('/')
-def hello():
-    secret_key = app.config.get("SECRET_KEY") 
-    return f"The skey is {secret_key}"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello, {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append(
+                    "Unable to get URL. Make sure it's valid and try again."
+            )
+            return render_template('index.html', errors=errors)
+        if r:
+            # text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/') # set path
+            nltk.download('punkt')
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save results
+            results = sorted(
+                    no_stop_words_count.items(),
+                    key=operator.itemgetter(1),
+                    reverse=True
+                    )[:10]
+            try:
+                result = PGapp(
+                        url=url,
+                        result_all=raw_word_count,
+                        result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
